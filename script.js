3dd4728e8c44d700c5ffd1efff37adf4ede54bf8
chriskimty <christaeyoung.kim@gmail.com>
2022-10-22 22:29:22 -0400

    added form functionality to contact form

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 //Create portfolio object
 const portfolio = {};
 
-// Hamburger Nav slide-out toggle
+// Hamburger Nav Function
 portfolio.hamNavToggle = () => {
+    // Hamburger nav slide-out toggle
     hamNavToggle = document.querySelector(".hamNavIcon");
     portfolio.hamNav = document.querySelector(".hamNav");
     
     hamNavToggle.addEventListener("click", () => {
         portfolio.hamNav.classList.toggle("openNav");
     });
-};
-
-// Remove Hamburger Nav from view once links are clicked
-portfolio.removeHamNav = () => {
+    // Remove Hamburger Nav from view once links are clicked
     const hamLinks = document.querySelectorAll(".hamLinks");
 
     hamLinks.forEach(hamLinks => {
@@ -23,8 +21,8 @@ portfolio.removeHamNav = () => {
 // Typewriter Function 
 portfolio.typeWriter = () => {
     const text = document.querySelector(".secondText");
-
-    const textLoad = () => {
+    
+    const textAnimation = () => {
         setTimeout(() => {
             text.textContent = "Musician"
         }, 0);
@@ -32,18 +30,64 @@ portfolio.typeWriter = () => {
             text.textContent = "Educator"
         }, 3000);
         setTimeout(() => {
-            text.textContent = "Learner"
+            text.textContent = "Creator"
         }, 6000);
-    }   
-    textLoad();
-    setInterval(textLoad, 9000);
-}
+        setTimeout(() => {
+            text.textContent = "Learner"
+        }, 9000);
+        setTimeout(() => {
+            text.textContent = "Cat Mom"
+        }, 12000);
+    }
+    textAnimation();
+    setInterval(textAnimation, 15001);
+    // changed from 15000, but technically, not sure why the logic is flawed since the entire function re-runs every 15 seconds... 
+};
+
+//Contact Form - Prevent Default Behavior
+portfolio.formSubmission = () => {
+    const form = document.getElementById("myForm");
+    const input = document.querySelectorAll(".input");
+    async function handleSubmit(event) {
+        event.preventDefault();
+        input.textContent = "";
+        const status = document.getElementById("myFormStatus");
+        const data = new FormData(event.target);
+
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                status.innerHTML = "Thank you for your message!";
+                status.classList.add("success")
+                form.reset()
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                    } else {
+                        status.innerHTML = "Oops! There was a problem. Please try again."
+                        status.classList.add("error")
+                    }
+                })
+            }
+        }).catch(error => {
+            status.innerHTML = "Oops! There was a problem. Please try again."
+            status.classList.add("error")
+        });
+    }
+    form.addEventListener("submit", handleSubmit)
+};
 
 //init function
 portfolio.init = () => {
     portfolio.hamNavToggle();
-    portfolio.removeHamNav();
     portfolio.typeWriter();
+    portfolio.formSubmission();
 }
 
 portfolio.init();
